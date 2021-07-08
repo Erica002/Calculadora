3cd6560a6f4957a3e0b5a66c85a4d0f38d2e9c6e
Erica002 <souzaericaregina22@gmail.com>
2021-07-08 12:18:40 -0300

    Display Calculadora utilizando component e props. Botões da calculadora como componente

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+import Buttons from './src/Buttons';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style = {styles.visor}>  </Text>
+      </View>
+          
+    <View style={styles.buttonDeleta}>
+      <Buttons text ='CE/C' deleta />
     </View>
+    <View style={styles.buttons}>  
+      <Buttons text = '7'  />
+      <Buttons text = '8'  />
+      <Buttons text = '9'  />
+      <Buttons text = '%'  />
+      <Buttons text = '>'  />
+      <Buttons text = '4'  />
+      <Buttons text = '5'  />
+      <Buttons text = '6'  />
+      <Buttons text = 'X'  />
+      <Buttons text = '÷'  />
+      <Buttons text = '1'  />
+      <Buttons text = '2'  />
+      <Buttons text = '3'  />
+      <Buttons text = '+'  />
+      <Buttons text = '-'  />
+      <Buttons text = '0' zero />
+      <Buttons text = '00' />
+      <Buttons text = '.'  />
+      <Buttons text = '='  />
+    </View>
+  </View>   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'white',
+    borderWidth: 1,
+    marginTop: 1,
+    marginBottom: 1,
+    marginLeft: 1,
   },
+
+  visor: {
+    height: 60,
+    width: 320,
+    padding: 20,
+    margin: 5,
+    borderRadius: 5,
+    color: 'black',
+    backgroundColor: 'gray',
+      
+  },
+  
+  buttonDeleta: {
+    alignSelf: 'flex-end',
+    marginRight: 18,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    borderRadius: 10,
+    marginLeft: 15,
+  },
+
+  botaoResultado: {
+    alignSelf: 'flex-end',
+    marginRight: 18, 
+  
+  },  
 });
---
+++ src/Buttons.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity} from 'react-native'
+
+
+export default props => {
+  const stylesButton = [styles.buttons]
+  if (props.deleta) stylesButton.push(styles.buttonDeleta)
+  if (props.zero) stylesButton.push(styles.buttonZero)
+  return (
+      <TouchableOpacity onPress={() => props.onClick(props.text)}>
+          <Text style={stylesButton}>{props.text}</Text>
+      </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+     
+    buttonDeleta: {
+        height: 65,
+        width: 65,
+        fontSize: 12,
+        borderRadius: 10,
+        color: 'white',
+        backgroundColor: '#FF8C00',
+    },
+
+    buttons: {
+        fontSize: 15,
+        height: 65,
+        width: 65,
+        padding: 19,
+        borderRadius: 10,
+        borderWidth: 4,
+        borderColor: 'black',
+        textAlign: 'center',
+        color: 'gray',
+        backgroundColor: '#363636',
+        
+       
+    },
+
+    buttonZero: {
+      fontSize: 15,
+      height: 65,
+      width: 128,
+      backgroundColor: '#363636',
+      textAlign: 'center',
+      color: 'gray',
+      borderColor: 'black',
+      borderRadius: 10,
+  },
+})
